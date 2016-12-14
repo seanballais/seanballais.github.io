860db1c5b3aa6591255cbd0d1ec5d15904975b90
seanballais <sean@seanballais.com>
2016-12-14 23:26:32 +0800

    Working on the contact page.

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,24 +1,7 @@
 $(function() {
-    var $contactResponse = $('div#contact-response');
-    var $contact = $('div#contact');
-    var $contactForm = $('div#contact > form#contact-form');
-    $contactForm.submit(function(e) {
+    $('#contact-form').on('submit', function(e) {
         e.preventDefault();
-        $.ajax({
-            url: '//formspree.io/sean@seanballais.com',
-            method: 'POST',
-            data: $(this).serialize(),
-            dataType: 'json',
-            beforeSend: function() {
-                $contactResponse.attr('class', 'alert alert-info').text('Sending message').fadeIn('slow');
-            },
-            success: function(data) {
-                $contactResponse.attr('class', 'alert alert-success').text('Message sent!').fadeIn('slow');
-                $contact.slideUp('slow');
-            },
-            error: function(err) {
-                $contactResponse.attr('class', 'alert alert-danger').text('Sorry, my bad. Something went wrong. Make sure you have Javascript enabled, and you are connected to the Internet.').fadeIn('slow');
-            }
-        });
+
+        alert("Submitting.");
     });
 });
---
+++ static/js/misc.js
@@ -0,0 +1,18 @@
+// make all images responsive
+$(function() {
+	$("img").addClass("img-responsive");
+});
+
+// responsive tables
+$(document).ready(function() {
+	$("table").wrap("<div class='table-responsive'></div>");
+	$("table").addClass("table");
+});
+
+// responsive embed videos
+$(document).ready(function () {
+    $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+	$('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
+    $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+	$('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
+});
